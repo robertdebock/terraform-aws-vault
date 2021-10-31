80f0888c8475843e266acaeea3aa3fb9bb96be78
Robert de Bock <robert@meinit.nl>
2021-10-31 21:24:50 +0100

    Plus one and minus one.

--- main.tf
+++ main.tf
@@ -249,8 +249,8 @@ resource "aws_lb_listener" "default" {
 resource "aws_autoscaling_group" "default" {
   name                      = var.name
   desired_capacity          = var.amount
-  min_size                  = var.amount
-  max_size                  = var.amount
+  min_size                  = var.amount - 1
+  max_size                  = var.amount + 1
   health_check_type         = "EC2"
   placement_group           = aws_placement_group.default.id
   max_instance_lifetime     = var.max_instance_lifetime
