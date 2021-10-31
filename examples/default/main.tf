9ae98dd98b3d99bcc84f3ad09aceddebc64a4afb
Robert de Bock <robert@meinit.nl>
2021-10-31 22:06:15 +0100

    3. + or - 1.

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -7,3 +7,11 @@ module "vault" {
     owner    = "robertdebock"
   }
 }
+
+# Recovery Key 1: I1u103yGctZ0bBN6V9qHSvdb5sRl2H+VA3eZWJKk8uLd
+# Recovery Key 2: teePy3qd2b6lG+GUQy6HcVvHlieHLbqPZuvdibKVpvCw
+# Recovery Key 3: 3nWytCTVKLmPsv0tioMTO+ztJfvYEgGdDKmhqDOuU8qI
+# Recovery Key 4: pSoqE51+KcXBuMIqWx3xk6DTeflRuy34PoWH05LiboBF
+# Recovery Key 5: ec6Dicvv1r3akcEjdc7TFbJqbQks2TEqVlVTJZNBx4ao
+#
+# Initial Root Token: s.J5bRa5XxFkb7KpymaajMsPsy
