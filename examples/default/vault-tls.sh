9c7f92b42aceaab46f30dd5a15da5207a83fbeba
Robert de Bock <robert@meinit.nl>
2021-11-03 12:15:01 +0100

    Less comments.

--- examples/default/vault-tls.sh
+++ examples/default/vault-tls.sh
@@ -25,13 +25,3 @@ keyUsage           = critical, digitalSignature, cRLSign, keyCertSign
 # Create CA key and certificate.
 openssl req -config <(echo "$CA_CONFIG") -new -newkey rsa:2048 -nodes \
  -subj "/CN=Snake Root CA" -x509 -extensions ext -keyout $TLS_PATH/vault_ca.pem -out $TLS_PATH/vault_ca.crt
-
-# # Create new private key and CSR
-# openssl req -config request.cfg -new -newkey rsa:2048 -nodes \
-#  -keyout $TLS_PATH/vault.pem -extensions ext -out $TLS_PATH/vault.csr
-#
-# # Sign the CSR
-# openssl x509 -extfile request.cfg -extensions ext -req -in $TLS_PATH/vault.csr -CA $TLS_PATH/vault_ca.crt -CAkey $TLS_PATH/vault_ca.pem -CAcreateserial -out $TLS_PATH/vault.crt -days 365
-#
-# # Concatenate CA and server certificate
-# cat $TLS_PATH/vault_ca.crt >> $TLS_PATH/vault.crt
