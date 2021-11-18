e9a552f16a7c8056f5e79559105bfdb6b4e9cf89
Robert de Bock <robert@meinit.nl>
2021-11-18 16:34:52 +0100

    Add and remove scenarios.

--- examples/development/main.tf
+++ examples/development/main.tf
@@ -1,6 +1,6 @@
 # Make a certificate.
 resource "aws_acm_certificate" "default" {
-  domain_name       = "vault.robertdebock.nl"
+  domain_name       = "ci-vault.robertdebock.nl"
   # After a deployment, this value (`domain_name`) can't be changed because the certificate is bound to the load balancer listener.
   validation_method = "DNS"
   tags = {
@@ -24,7 +24,8 @@ resource "cloudflare_record" "validation" {
 # Call the module.
 module "vault" {
   source          = "../../"
-  name            = "vltp"
+  size            = "development"
+  spot_price      = 0.024
   certificate_arn = aws_acm_certificate.default.arn
   tags = {
     owner = "robertdebock"
@@ -34,7 +35,7 @@ module "vault" {
 # Add a loadbalancer record to DNS zone.
 resource "cloudflare_record" "default" {
   zone_id = data.cloudflare_zone.default.id
-  name    = "vault"
+  name    = "ci-vault"
   value   = module.vault.aws_lb_dns_name
   type    = "CNAME"
 }
