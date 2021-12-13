e95dc7021b4cbf2d78d1c0b64e4728351a873845
Robert de Bock <robert@meinit.nl>
2021-12-13 15:04:18 +0100

    Terminate list.

--- variables.tf
+++ variables.tf
@@ -1,13 +1,4 @@
-variable "name" {
-  description = "The name of the vault cluster in 3 to 5 characters."
-  type        = string
-  default     = "vault"
-  validation {
-    condition     = length(var.name) >= 3 && length(var.name) <= 5 && var.name != "default"
-    error_message = "Please use a minimum of 3 and a maximum of 5 characters. \"default\" can't be used because it is reserved."
-  }
-}
-
+t
 variable "vault_version" {
   description = "The version of Vault to install."
   type        = string
@@ -28,7 +19,7 @@ variable "region" {
   type        = string
   default     = "eu-west-1"
   validation {
-    condition     = contains(["eu-central-1", "eu-north-1", "eu-south-1", "eu-west-1", "eu-west-2", "eu-west-3", ], var.region)
+    condition     = contains(["eu-central-1", "eu-north-1", "eu-south-1", "eu-west-1", "eu-west-2", "eu-west-3" ], var.region)
     error_message = "Please use \"eu-central-1\", \"eu-north-1\", \"eu-south-1\", \"eu-west-1\", \"eu-west-2\" or \"eu-west-3\"."
   }
 }
