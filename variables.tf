3dd4ca52f29ca60ba018fe9802cd5e05e4f6e761
Robert de Bock <robert@meinit.nl>
2022-03-24 17:02:56 +0100

    Add all regions.

--- variables.tf
+++ variables.tf
@@ -33,8 +33,8 @@ variable "region" {
   type        = string
   default     = "eu-west-1"
   validation {
-    condition     = contains(["eu-central-1", "eu-north-1", "eu-south-1", "eu-west-1", "eu-west-2", "eu-west-3", ], var.region)
-    error_message = "Please use \"eu-central-1\", \"eu-north-1\", \"eu-south-1\", \"eu-west-1\", \"eu-west-2\" or \"eu-west-3\"."
+    condition     = contains(["af-south-1", "ap-east-1", "ap-northeast-1", "ap-northeast-2", "ap-northeast-3", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "ap-southeast-3", "ca-central-1", "eu-central-1", "eu-north-1", "eu-south-1", "eu-west-1", "eu-west-2", "eu-west-3", "me-south-1", "sa-east-1", "us-east-1", "us-east-2", "us-west-1", "us-west-2"], var.region)
+    error_message = "Please use a valid region."
   }
 }
 
