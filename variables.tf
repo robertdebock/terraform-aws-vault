900cf53c958eb60aaa844ef5299a925308942dbe
Robert de Bock <robert@meinit.nl>
2022-05-12 06:23:08 +0200

    Update version.

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "name" {
 variable "vault_version" {
   description = "The version of Vault to install."
   type        = string
-  default     = "1.10.0"
+  default     = "1.10.3"
   validation {
     condition     = can(regex("^1\\.", var.vault_version))
     error_message = "Please use a SemVer version, where the major version is \"1\". Use \"1.2.7\" or newer."
