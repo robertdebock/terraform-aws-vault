f3e584ef8ba7b6bf6cfd314cebeacb999dc92c92
Robert de Bock <robert@meinit.nl>
2022-05-20 08:34:26 +0200

    Explain a little more on the provider AWS credenials.

--- providers.tf
+++ providers.tf
@@ -1,3 +1,8 @@
 provider "aws" {
+  # You can set your AWS credentials in environment variables:
+  # ```shell
+  # export AWS_ACCESS_KEY_ID=AKIA123ABC
+  # export AWS_SECRET_ACCESS_KEY=123ABC
+  # ```
   region = var.region
 }
