5850bf74735313fd5c0caf423cdf7e9802398114
Robert de Bock <robert@meinit.nl>
2022-07-04 11:27:00 +0200

    Update provider versions.

--- examples/development/versions.tf
+++ examples/development/versions.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.21.0"
+    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "3.9.1"
+      version = "3.18.0"
     }
   }
 }
--- versions.tf
+++ versions.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.9.0"
+      version = "4.21.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "2.2.2"
+      version = "2.2.3"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.1.2"
+      version = "3.3.2"
     }
   }
 }
