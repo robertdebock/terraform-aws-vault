8ef468a3a0e38b54aff5e288f34da5bdae08e98a
repping <r.eppingbroek@outlook.com>
2022-08-29 16:21:19 +0200

    init cloudwatch example

---
+++ examples/cloudwatch/providers.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+provider "cloudflare" {
+  api_token = "unset"
+}
---
+++ examples/cloudwatch/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.27.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.22.0"
+    }
+  }
+}
