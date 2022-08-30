f688dc3fc36587e2023ae3d1f7f2d8aaf5acc89f
Robert de Bock <robert@meinit.nl>
2022-08-30 21:26:12 +0200

    output AWS details now.

--- examples/replication/outputs.tf
+++ examples/replication/outputs.tf
@@ -1,16 +1,11 @@
-output "vault_urls_us" {
-  description = "The URL to this Vault installation."
-  value       = cloudflare_record.api_us.*.hostname
-}
-
 output "vault_urls_eu" {
   description = "The URL to this Vault installation."
-  value       = cloudflare_record.api_eu.*.hostname
+  value       = aws_route53_record.eu.*.name
 }
 
-output "instructions_us" {
-  description = "How to initialize Vault."
-  value       = module.vault_us.*.instructions
+output "vault_urls_us" {
+  description = "The URL to this Vault installation."
+  value       = aws_route53_record.us.*.name
 }
 
 output "instructions_eu" {
@@ -18,7 +13,12 @@ output "instructions_eu" {
   value       = module.vault_eu.*.instructions
 }
 
+output "instructions_us" {
+  description = "How to initialize Vault."
+  value       = module.vault_us.*.instructions
+}
+
 output "vault_url" {
   description = "The global endpoint, routing to the nearest active Vault cluster."
-  value       = "https://vault.my_company.com:8200/"
+  value       = "https://vault.meinit.nl`:8200/"
 }
