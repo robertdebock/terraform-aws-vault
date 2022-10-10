a1330a467366b914faf036bf294e3a6bcb722d7b
Robert de Bock <robert@meinit.nl>
2022-10-10 10:56:53 +0200

    Typos fixed.

--- variables.tf
+++ variables.tf
@@ -191,7 +191,7 @@ variable "vault_private_subnet_ids" {
 }
 
 variable "vault_public_subnet_ids" {
-  description = "The ids of the private subnets to deploy to. These subnets should have in internet gateway. ßOnly required when `vault_aws_vpc_id` is set."
+  description = "The ids of the public subnets to deploy the load balancer to. Only required when `vault_aws_vpc_id` is set."
   type        = list(string)
   default     = []
 }
