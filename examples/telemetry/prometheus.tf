21e7e20156c586943638e7d62e3c974ea1d65b8e
Robert de Bock <robert@meinit.nl>
2022-10-12 11:53:15 +0200

    Obfuscate.

--- examples/telemetry/prometheus.tf
+++ examples/telemetry/prometheus.tf
@@ -6,7 +6,7 @@ resource "local_file" "prometheus" {
   content = templatefile("${path.module}/user_data_prometheus.sh.tpl",
     {
       # You don't know the `credenial` before initializing Vault.
-      credenial = "hvs.lkFuVPHlbLlHRq0x5Sahp5Cz"
+      credenial = "hvs.OBFUSCATEDOBFUSCATEDOBFU"
       target    = aws_route53_record.default.fqdn
     }
   )
