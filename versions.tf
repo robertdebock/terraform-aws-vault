55396777f62d515f421482b2ca2739fd5cdc764e
Robert de Bock <robert@meinit.nl>
2022-11-18 09:06:43 +0100

    A little more relaxed on the permissions.

--- versions.tf
+++ versions.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.39.0"
+      version = ">= 4.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.2.3"
+      version = ">= 2.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.4.3"
+      version = ">= 3.0"
     }
   }
 }
