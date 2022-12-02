df57090cf15bfc59141705eda95438648123e6fd
Robert de Bock <robert@meinit.nl>
2022-12-02 13:49:49 +0100

    Split output

--- examples/replication/outputs.tf
+++ examples/replication/outputs.tf
@@ -1,12 +1,23 @@
-output "vault_urls_eu" {
-  description = "The URL to this Vault installation."
-  value       = aws_route53_record.eu.*.name
+output "vault_url_eu_0" {
+  description = "value"
+  value       = "https://${module.vault_eu[0].aws_lb_replication_dns_name}:8200"
 }
 
-output "vault_urls_us" {
-  description = "The URL to this Vault installation."
-  value       = aws_route53_record.us.*.name
+output "vault_url_eu_1" {
+  description = "value"
+  value       = "https://${module.vault_eu[1].aws_lb_replication_dns_name}:8200"
 }
+
+output "vault_url_us_0" {
+  description = "value"
+  value       = "https://${module.vault_us[0].aws_lb_replication_dns_name}:8200"
+}
+
+output "vault_url_us_1" {
+  description = "value"
+  value       = "https://${module.vault_us[1].aws_lb_replication_dns_name}:8200"
+}
+
 
 output "instructions_eu" {
   description = "How to initialize Vault."
@@ -18,7 +29,17 @@ output "instructions_us" {
   value       = module.vault_us.*.instructions
 }
 
-output "vault_url" {
+output "vault_url_global" {
   description = "The global endpoint, routing to the nearest active Vault cluster."
-  value       = "https://vault.meinit.nl:8200/"
+  value       = "https://${aws_route53_record.global_endpoint_eu.name}:8200/"
+}
+
+output "vault_url_eu" {
+  description = "The EU endpoint, routing to the active Vault cluster."
+  value       = "https://${aws_route53_record.eu[0].name}:8200/"
+}
+
+output "vault_url_us" {
+  description = "The US endpoint, routing to the active Vault cluster."
+  value       = "https://${aws_route53_record.us[0].name}:8200/"
 }
