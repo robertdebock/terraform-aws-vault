ad4668cef0c51eb3af063c2cd6c83f8e39c60327
Robert de Bock <robert@meinit.nl>
2022-12-08 16:52:01 +0100

    A bit cleaner.

--- examples/cleanup.sh
+++ examples/cleanup.sh
@@ -2,19 +2,32 @@
 
 # A script to cleanup all local files and regenerate files required.
 
+delete_object() {
+  if [ -z "${1}" ] ; then
+    echo "Please pass an object to this function."
+    exit 1
+  fi
+  
+  if [ -f "${1}" ] ; then
+     echo "Removing file ${1}."
+     rm "${1}"
+  fi
+
+  if [ -d "${1}" ] ; then
+    echo "Removing directory ${1}."
+    rm -Rf "${1}"
+  fi
+}
+
 for scenario in * ; do
   if [ -d "${scenario}" ] ; then
-    echo "Working on scenario: ${scenario}."
-    cd "${scenario}"
+    echo "Working on scenario: ${scenario}." ; cd "${scenario}"
     if [ -d prerequisites ] ; then
       echo "Working on prerequisites."
       cd prerequisites
       terraform destroy -auto-approve
-      for file in terraform.tfstate terraform.tfstate.backup ; do
-        test -f "${file}" && (echo "Removing file ${file}." ; rm "${file}")
-      done
-      for directory in .terraform ; do
-        test -d "${directory}" && (echo "Removing directory ${directory}" ; rm -Rf "${directory}")
+      for object in terraform.tfstate terraform.tfstate.backup .terraform ; do
+        delete_object "${object}"
       done
       echo "Running terraform init."
       terraform init
@@ -22,11 +35,8 @@ for scenario in * ; do
       terraform fmt
     fi
     terraform destroy -auto-approve
-    for file in id_rsa id_rsa.pub init.txt terraform.tfstate terraform.tfstate.backup .terraform.lock.hcl ; do
-      test -f "${file}" && (echo "Removing ${file}." ; rm "${file}")
-    done
-    for directory in .terraform ; do
-      test -d "${directory}" && (echo "Removing directory ${directory}" ; rm -Rf "${directory}")
+    for file in id_rsa id_rsa.pub init.txt terraform.tfstate terraform.tfstate.backup .terraform.lock.hcl .terraform ; do
+      delete_object "${object}"
     done
     echo "Generating ssh-key."
     ssh-keygen -b 2048 -f id_rsa -q -N ""
