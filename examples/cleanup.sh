5572fc8ec2a0cf5b30e1715e1bb6474785c52890
Robert de Bock <robert@meinit.nl>
2022-12-08 16:45:50 +0100

    Clean prerequites.

--- examples/cleanup.sh
+++ examples/cleanup.sh
@@ -4,16 +4,37 @@
 
 for scenario in * ; do
   if [ -d "${scenario}" ] ; then
+    echo "Working on scenario: ${scenario}."
     cd "${scenario}"
-    for file in id_rsa id_rsa.pub init.txt terraform.tfstate terraform.tfstate.backup .terraform.lock.hcl user_data.sh bastion_user_data.sh ; do
-      test -f "${file}" && rm "${file}"
+    if [ -d prerequisites ] ; then
+      echo "Working on prerequisites."
+      cd prerequisites
+      terraform destroy -auto-approve
+      for file in terraform.tfstate terraform.tfstate.backup ; do
+        test -f "${file}" && (echo "Removing file ${file}." ; rm "${file}")
+      done
+      for directory in .terraform ; do
+        test -d "${directory}" && (echo "Removing directory ${directory}" ; rm -Rf "${directory}")
+      done
+      echo "Running terraform init."
+      terraform init
+      echo "Running terraform fmt."
+      terraform fmt
+    fi
+    terraform destroy -auto-approve
+    for file in id_rsa id_rsa.pub init.txt terraform.tfstate terraform.tfstate.backup .terraform.lock.hcl ; do
+      test -f "${file}" && (echo "Removing ${file}." ; rm "${file}")
     done
     for directory in .terraform ; do
-      test -d "${directory}" && rm -Rf "${directory}"
+      test -d "${directory}" && (echo "Removing directory ${directory}" ; rm -Rf "${directory}")
     done
+    echo "Generating ssh-key."
     ssh-keygen -b 2048 -f id_rsa -q -N ""
+    echo "Generate TLS material."
     ./vault-tls.sh
+    echo "Running terraform init."
     terraform init
+    echo "Running terraform fmt."
     terraform fmt
     cd ../
   fi
