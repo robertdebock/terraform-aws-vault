743facacc0cf6bd0e0951a9c304ae3d181dc0cbf
Robert de Bock <robert@meinit.nl>
2022-12-21 10:29:14 +0100

    Explicitly block public S3 access.

--- s3.tf
+++ s3.tf
@@ -4,6 +4,15 @@ resource "aws_s3_bucket" "default" {
   tags   = local.scripts_tags
 }
 
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket = aws_s3_bucket.default.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   bucket = aws_s3_bucket.default.bucket
   rule {
