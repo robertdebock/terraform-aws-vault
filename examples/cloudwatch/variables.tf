bb6268e220924dab19851e3345a14d1d7b1b2573
repping <r.eppingbroek@outlook.com>
2023-01-06 09:52:40 +0100

    fix/removed unwanted var

--- examples/cloudwatch/variables.tf
+++ examples/cloudwatch/variables.tf
@@ -2,3 +2,12 @@ variable "domain" {
   default     = "meinit.nl"
   description = "The domain to use for the deployment. This domain should be hosted on AWS."
 }
+
+variable "region" {
+  default     = "eu-west-1"
+  description = "The region to use for the deployment."
+}
+
+variable "license" {
+  description = "Vault Enterprise license"
+}
